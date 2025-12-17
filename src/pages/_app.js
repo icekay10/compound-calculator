dacc22250498ea09f44c0fff53c414cfbdb3b0cc
icekay10 <isatakamara1001@gmail.com>
2025-12-17 21:37:57 +0200

    new

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import './globals.css';
 
 export default function App({ Component, pageProps }) {
