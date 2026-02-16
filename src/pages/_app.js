28875cb71a73fac5998c5cc01156bfc2e0a24b43
jabba10 <sahrjabba100@gmail.com>
2026-02-16 17:56:50 +0200

    ALLAH

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import './globals.css';
 
 export default function App({ Component, pageProps }) {
